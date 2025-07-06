d0fb6bdde67882937b90e5de4ed0c74a8040b120
Mehedi01616720009 <mehedi01616720009@gmail.com>
2025-07-06 17:24:53 +0600

    inproduct create validation update

--- src/modules/inproduct/inproduct.validation.ts
+++ src/modules/inproduct/inproduct.validation.ts
@@ -3,11 +3,6 @@ import { z } from 'zod';
 // create in product validation
 const createInProductValidationSchema = z.object({
     body: z.object({
-        dsr: z
-            .string({
-                required_error: 'Dsr is required',
-            })
-            .min(1, { message: 'Dsr cannot be empty' }),
         warehouse: z
             .string({
                 required_error: 'Warehouse is required',
